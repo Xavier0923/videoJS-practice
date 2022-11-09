1c7d11e2ae18d3a514c97add4e3549259fce4fc8
wei.zeng <wei.zeng@ctitv.com.tw>
2022-11-09 18:08:32 +0800

    first commit

---
+++ src/app/video-player/video-player.component.ts
@@ -0,0 +1,46 @@
+import { Component, ElementRef, Input, OnInit, ViewChild, Renderer2 } from '@angular/core';
+import videojs from 'video.js';
+
+@Component({
+  selector: 'app-video-player',
+  templateUrl: './video-player.component.html',
+  styleUrls: ['./video-player.component.scss']
+})
+export class VideoPlayerComponent implements OnInit {
+  @ViewChild('target', { static: true }) target!: ElementRef;
+  // See options: https://videojs.com/guides/options
+  @Input() options!: {
+    aspectRatio: string,
+    autoplay: boolean,
+    sources: {
+      src: string,
+      type: string,
+    }[],
+    controls: boolean,
+    playbackRates: number[]
+  };
+
+  player!: videojs.Player;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    this.player = videojs(this.target.nativeElement, this.options);
+    // 刪除字母畫面
+    const pictureInPicture = this.player.controlBar.getChild('pictureInPictureToggle')!;
+    this.player.controlBar.removeChild(pictureInPicture)
+    // 加入空白div
+    this.player.controlBar.addChild('div');
+    this.player.ready(() => {
+      console.log('asd')
+    })
+  }
+
+  // Dispose the player OnDestroy
+  ngOnDestroy() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
+
+}
