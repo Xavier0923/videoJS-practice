83b98fc7db11971d6a7336eb49992c3fd87d3a76
wei.zeng <wei.zeng@ctitv.com.tw>
2022-11-10 18:21:13 +0800

    revise files

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,5 +1,10 @@
-import { Component, ElementRef, Input, OnInit, ViewChild, Renderer2 } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild, Renderer2, enableProdMode } from '@angular/core';
 import videojs from 'video.js';
+import "videojs-hotkeys";
+import 'videojs-watermark';
+import 'videojs-thumbnails';
+
+declare var videojs: any;
 
 @Component({
   selector: 'app-video-player',
@@ -17,23 +22,26 @@ export class VideoPlayerComponent implements OnInit {
       type: string,
     }[],
     controls: boolean,
-    playbackRates: number[]
+    playbackRates: number[],
+    plugins: any,
   };
+
 
   player!: videojs.Player;
 
-  constructor(private renderer: Renderer2) { }
+  constructor() { }
 
   ngOnInit(): void {
+    videojs
     this.player = videojs(this.target.nativeElement, this.options);
+
     // 刪除字母畫面
     const pictureInPicture = this.player.controlBar.getChild('pictureInPictureToggle')!;
     this.player.controlBar.removeChild(pictureInPicture)
     // 加入空白div
     const playbackbtn = this.player.controlBar.getChild('playbackRateMenuButton')!;
     const playbackindex = this.player.controlBar.children().indexOf(playbackbtn);
-    this.player.controlBar.addChild('component', {style: 'flex: 1;'}, playbackindex);
-    console.log(this.player.controlBar);
+    // this.player.controlBar.addChild('div', {style: 'flex: 1'}, playbackindex)
     this.player.ready(() => {
       console.log('asd')
     })
